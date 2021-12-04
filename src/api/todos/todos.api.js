8b078e288ae78df3642cfc2cb30f60b3e4ddd4e6
marcfernandes <marcfernandes@teksystems.com>
2021-12-04 10:45:56 -0500

    another commit

--- src/api/todos/todos.api.js
+++ src/api/todos/todos.api.js
@@ -1,9 +1,45 @@
 export const CACHE_NAME = "todos";
 
-export const getAllTodos = () => {
+export const getAllTodos = async () => {
   return fetch(`${process.env.REACT_APP_API_SERVER}/task`, {
     headers: {
       Authorization: `${process.env.REACT_APP_BEARER}`,
     },
   }).then((res) => res.json());
+};
+
+export const getTodo = async (id) => {
+  return fetch(`${process.env.REACT_APP_API_SERVER}/task/${id}`, {
+    headers: {
+      Authorization: `${process.env.REACT_APP_BEARER}`,
+    },
+  }).then((res) => res.json());
+};
+
+export const createTodo = async (text) => {
+  fetch(`${process.env.REACT_APP_API_SERVER}/task`, {
+    method: "POST",
+    headers: {
+      Authorization: `${process.env.REACT_APP_BEARER}`,
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ text }),
+  }).then((res) => res.json());
+};
+
+export const updateTodo = async (todo) => {
+  fetch(`${process.env.REACT_APP_API_SERVER}/task`, {
+    method: "PUT",
+    headers: {
+      Authorization: `${process.env.REACT_APP_BEARER}`,
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  }).then((res) => res.json());
+};
+
+export const deleteTodo = async (id) => {
+  fetch(`${process.env.REACT_APP_API_SERVER}/task/${id}`, {
+    method: "DELETE",
+  }).then(() => id);
 };
--- src/pages/todos-react-query/todos-react-query.page.jsx
+++ src/pages/todos-react-query/todos-react-query.page.jsx
@@ -1,13 +1,56 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { CACHE_NAME, getAllTodos } from "../../api/todos/todos.api";
 import PageTitle from "../../components/page-title.component";
-import TodosReactQueryList from "./todos-react-query-list.component";
+import { SpinnerContext } from "../../providers/spinner.provider";
+import styled from "styled-components";
+
+const MainPanel = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const TodosReactQuery = () => {
+  const { isLoading, error, data } = useQuery(CACHE_NAME, getAllTodos);
+  const { setIsLoading } = useContext(SpinnerContext);
+  const [openAdd, toggleOpenAdd] = useState(false);
+
+  const onToggleOpenAdd = () => {
+    toggleOpenAdd(!openAdd);
+  };
+
+  useEffect(() => {
+    setIsLoading(isLoading);
+  }, [setIsLoading, isLoading]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  const { count, data: todos } = data;
+
   return (
-    <div>
-      <PageTitle title="React Query Todos" />
-      <TodosReactQueryList />
-    </div>
+    <MainPanel>
+      <PageTitle title={`React Query Todos - count ${count}`} />
+      <div>
+        <ul>
+          {todos.map(({ _id, description, completed }) => (
+            <li key={_id}>
+              <h3>
+                {_id} - {description} - {completed ? "Done" : "To be done"}
+              </h3>
+            </li>
+          ))}
+        </ul>
+        <button onClick={onToggleOpenAdd}>Add</button>
+      </div>
+    </MainPanel>
   );
 };
 
